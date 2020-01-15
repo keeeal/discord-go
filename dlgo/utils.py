de7fe7adb8d78cc7a8956785f04850f4a136c415
keeeal <james@keal.com.au>
2020-01-15 19:56:54 +1030

    play against random bot

--- dlgo/utils.py
+++ dlgo/utils.py
@@ -17,18 +17,20 @@ def print_move(player, move):
         move_str = 'resigns'
     else:
         move_str = '%s%d' % (COLS[move.point.col - 1], move.point.row)
-    print('%s %s' % (player, move_str))
+    return '%s: %s' % (player, move_str)
 
 
 def print_board(board):
+    lines = []
     for row in range(board.num_rows, 0, -1):
         bump = " " if row <= 9 else ""
         line = []
         for col in range(1, board.num_cols + 1):
             stone = board.get(gotypes.Point(row=row, col=col))
             line.append(STONE_TO_CHAR[stone])
-        print('%s%d %s' % (bump, row, ''.join(line)))
-    print('    ' + '  '.join(COLS[:board.num_cols]))
+        lines.append('%s%d %s' % (bump, row, ''.join(line)))
+    lines.append('    ' + '  '.join(COLS[:board.num_cols]))
+    return '```\n' +'\n'.join(lines) + '\n```'
 # end::print_utils[]
 
 
--- run.py
+++ run.py
@@ -2,19 +2,50 @@
 import discord
 from discord.ext import commands
 
-def main():
-    with open('token.txt') as f:
-        token = f.readline()[:-1]
+from dlgo import agent
+from dlgo import goboard_slow as goboard
+from dlgo import gotypes
+from dlgo.utils import *
 
-    bot = commands.Bot(command_prefix='!')
+with open('token.txt') as f:
+    token = f.readline()[:-1]
 
-    @bot.command()
-    async def play(context):
-        await context.send("Let's play go!")
+board_size = 13
+human = gotypes.Player.black
+client = commands.Bot(command_prefix='')
+game = goboard.GameState.new_game(board_size)
+bot = agent.RandomBot()
 
-    bot.run(token)
+@client.event
+async def on_ready():
+    print('Logged in as', client.user.name)
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    main(**vars(parser.parse_args()))
+@client.command()
+async def show(context):
+    await context.send(print_board(game.board))
+
+@client.command()
+async def play(context, *args):
+    global game
+
+    if game.next_player is human:
+
+        # human's turn
+        point = point_from_coords(''.join(args).upper())
+        move = goboard.Move.play(point)
+        await context.send(print_move(game.next_player, move))
+        game = game.apply_move(move)
+        await context.send(print_board(game.board))
+
+        while game.next_player is not human:
+
+            # bot's turn(s)
+            move = bot.select_move(game)
+            await context.send(print_move(game.next_player, move))
+            game = game.apply_move(move)
+            await context.send(print_board(game.board))
+
+    else:
+        await context.send('It is not your turn yet...')
+
+client.run(token)
